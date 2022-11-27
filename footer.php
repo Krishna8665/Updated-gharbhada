ff378d9a691d6a732aa339432b836e18698e2358
Krishna8665 <117195015+Krishna8665@users.noreply.github.com>
2022-11-27 22:13:45 +0545

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,45 +1,53 @@
-<section id="about">
-<table id="tab" >
-	<tr>
-		<th>
-	        <h1 id="headings">
-					<a href="about.html"><img src="img/logo.png" alt="ghar bhada logo" id="bottom" class="logo"></a><br>
-				</h1>
-				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
-				<div class="wrapper">
-				  <div class="button">
-					 <div class="icon">
-						<a href=""><i class="fab fa-facebook-f"></i></a>
-					 </div>
-				  </div>
-				  <div class="button">
-					 <div class="icon">
-						<a href=""><i class="fab fa-twitter"></i></a>
-					 </div>
-				  </div>
-				  <div class="button">
-					 <div class="icon">
-						<a href="www.instagram.com"><i class="fab fa-instagram"></i></a>
-					 </div>
-				  </div>
-				  <div class="button">
-					 <div class="icon">
-						<a href="www.linkedin.com/in/rajiv-parajuli-39b449255"><i class="fab fa-linkedin"></i></a> 
-					 </div>
-				  </div>
-			   </div>  
-		</th>
-		<th>
-			<p class="describe">Based on pokhara valley, Ghar Bhada is a Room rental webapp</p>
-			<p class="describe">that helps people find their desired accomodation.</p><br><br><br><br>
-		</th>
-		<th>
-			<h2 class="main">Contact us</h1>
-			<p class="small">Masbar, Pokhara, Nepal</p>
-			<p class="small">9999999999</p> 
-			<p><a class="linkss" href="mailto: be2019ce11@gces.np">be2019ce11@gces.np</a></p>
-		</th>
-	</tr>
-</table>
-<p class="describe">&copy 2022 GharBhada . All Rights Reserved</p>
-</section>
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="footer-in">
+            <p class="mb-0">&copy 2022 GharBhada . All Rights Reserved.</p>
+        </div>
+    </div>
+</footer>
+
+
+
+
+</div>
+
+</div>
+
+
+
+<!-------complete html----------->
+
+
+
+
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script>
+$(document).ready(function() {
+    $('#myTable').dataTable();
+});
+</script>
+
+<script type="text/javascript">
+$(document).ready(function() {
+    $(".xp-menubar").on('click', function() {
+        $("#sidebar").toggleClass('active');
+        $("#content").toggleClass('active');
+    });
+
+    $('.xp-menubar,.body-overlay').on('click', function() {
+        $("#sidebar,.body-overlay").toggleClass('show-nav');
+    });
+
+});
+</script>
+
+
+
+
+
+</body>
+
+</html>
